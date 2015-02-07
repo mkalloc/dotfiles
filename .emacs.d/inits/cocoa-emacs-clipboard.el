054295ea2896ab959fbf7214d62f2be62582488c
mkalloc <mkalloc.dev@gmail.com>
2015-02-07 13:52:06 +0900

    ADD: darwin clipboard

---
+++ .emacs.d/inits/cocoa-emacs-clipboard.el
@@ -0,0 +1,15 @@
+;;clipboard
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+        (process-send-string proc text)
+        (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+;;buffer with clipboard
+(global-set-key "\C-w" 'clipboard-kill-region)
+(global-set-key "\M-w" 'clipboard-kill-ring-save)
+(global-set-key "\C-y" 'clipboard-yank)
