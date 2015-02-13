765f4837e9dc2f8af753533dab46c3972307d989
mkalloc <mkalloc.dev@gmail.com>
2015-02-13 22:37:06 +0900

    ADD: commonlisp.el

---
+++ package/commonlisp/03-commonlisp.el
@@ -0,0 +1,19 @@
+(require 'slime)
+(autoload 'slime "slime" nil t)
+(autoload 'slime-mode "slime" nil t)
+(add-to-list 'auto-mode-alist '("\\.lisp$" . lisp-mode))
+(add-to-list 'auto-mode-alist '("\\.cl$" . lisp-mode))
+(add-to-list 'interpreter-mode-alist '("sbcl" . lisp-mode))
+
+(require 'ac-slime)
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+
+(add-hook 'lisp-mode-hook
+          '(lambda ()
+             (unless (get-buffer "*slime-repl sbcl*")
+                 (progn
+                   (setq inferior-lisp-program "sbcl")
+                   (slime-mode)
+                   (slime-setup '(slime-repl slime-fancy slime-banner))
+                   (slime)))))
