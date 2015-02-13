0982b96452800bc0e5a36da525c709bded78e106
mkalloc <mkalloc.dev@gmail.com>
2015-02-13 22:50:39 +0900

    FIX: cd

--- package/commonlisp.zsh
+++ package/commonlisp.zsh
@@ -1,15 +1,9 @@
 #!/usr/bin/env zsh
-set -e
-
-# begin
-###################################################
-CURRENT_DIR=$(pwd)
-cd $SHELL_HOME/temp
 
 # set static files
 ###################################################
 cl=$(absPath ./commonlisp/03-commonlisp.el)
-[ ! -e "$cl" ] && cl=$(absPath ./package/commonlisp/03-commonlisp.el)
+[ ! -e "$cl" ] && cl=$(absPath ./commonlisp/03-commonlisp.el)
 ln -sf $cl $HOME/.emacs.d/inits/
 
 # install sbcl
@@ -32,10 +26,3 @@ fi
 ###################################################
 # curl -L http://common-lisp.net/project/slime/snapshots/slime-current.tgz | tar x
 # mv slime-* ~/.emacs.d/slime
-
-
-# finally
-###################################################
-cd $CURRENT_DIR
-
-set +e
