3571484fcd824650fe7f6263664b85d5973dd852
mkalloc <mkalloc.dev@gmail.com>
2015-03-24 20:30:53 +0900

    ADD: print message go.zsh

--- package/go.zsh
+++ package/go.zsh
@@ -2,18 +2,22 @@
 set -e
 # set static files
 ###################################################
+echo "===> set ENV"
 add-export GOPATH=$SHELL_HOME/temp/go
 mkdir -p $GOPATH
 add-export PATH=$GOPATH/bin:$PATH
+echo "===> set el"
 goel=$(absPath ./go/03-go.el)
 [ ! -e "$goel" ] && goel=$(absPath ./package/go/03-go.el)
 ln -sf $goel $HOME/.emacs.d/inits/
 # install go
 ###################################################
 if hasCmd 'go'; then
+  echo "===> skip install"
 else
 # os
 ###################################################
+  echo "===> install go"
   case ${OSTYPE} in
     darwin*)
       brew install go
@@ -23,7 +27,7 @@ else
       ;;
   esac
 fi
-
+echo "===> get packages"
 go get -u github.com/nsf/gocode
 go get -u code.google.com/p/rog-go/exp/cmd/godef
 go get -u github.com/golang/lint/golint
